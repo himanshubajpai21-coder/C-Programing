5d148adca24a80ff6d4576030427d4db7594c79d
HIMANSHU BAJPAI <himanshubajpai.16445@gmail.com>
2026-09-02 10:07:31 +0000

    Add program to calculate the area of a square

---
+++ prgm7.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+  
+    float a;
+
+    printf("ENTER THE SIDE OF A SQUARE : ");
+    scanf("%f",&a);
+
+
+    printf("AREA OF SQUARE IS :%d",a*a);
+
+
+
+     return 0;
+}
