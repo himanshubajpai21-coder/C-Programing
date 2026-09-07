f2bb13506c802f9b4db7e3c7005d50eeae1666e0
HIMANSHU BAJPAI <himanshubajpai.16445@gmail.com>
2026-09-07 09:31:54 +0000

    Add program to check if a number is divisible by two

---
+++ prgm9.c
@@ -0,0 +1,40 @@
+// WRITE A PROGRAM TO SEE WHETHER THE NUMBER IS DIVISIBLE BY TWO OR NOT 
+
+# include <stdio.h>
+
+int main()
+{
+
+    int a;
+
+    printf("ENTER A NUMBER : ");
+    scanf("%d",&a);
+
+    if (a%2==0){
+
+        printf("THE NUMBER IS DIVISIBLE BY 2");
+    
+    }
+
+    else
+    {
+         printf("THE NUMBER IS NOT DIVISIBLE BY 2");
+
+    }
+
+return 0 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
