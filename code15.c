841fb43c119134dfdd26b66b52253e2ad877a241
HIMANSHU BAJPAI <himanshubajpai.16445@gmail.com>
2026-09-17 06:12:19 +0000

    Add program to check if numbers are even or odd

---
+++ code15.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+  int a;
+
+  printf("ENTER THE 10 NUMBERS : ");
+
+for(int i =0; i,10;i++)
+{
+    scanf("%d",&a);
+    if(a%2==0)
+   {
+   printf("number is even\n");
+   }
+   else
+   {
+   printf("number is odd\n");
+   }
+ }
+return 0;
+}
